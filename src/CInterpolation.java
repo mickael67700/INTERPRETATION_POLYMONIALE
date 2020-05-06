ed7a200a0b3ad01f79969fb1a1e8c93d63d8b72c
Mike <mickael.merlet67@gmail.com>
2020-05-06 10:01:07 +0200

    Méthode interpolation()

--- src/CInterpolation.java
+++ src/CInterpolation.java
@@ -58,4 +58,90 @@ public class CInterpolation {
             tabpts_a_interpoler[i] = Point;
         }
     }
-}
+
+    void interpolation(){
+        int i, j, k;
+        double terme;
+        CPoint Point_Connu = new CPoint();
+        CPoint Point_Connu_i = new CPoint();
+        CPoint Point_Connu_j = new CPoint();
+        //Allocation dynamique tableau des diffs divisées
+        double tab_diff[] = new double[nbpts_connus];
+
+        //Allocation dynamique du tableau des coefficients
+        double tab_coef[] = new double[nbpts_connus];
+
+        //Initialisation de la table des diff divisées
+        for ( i = 0; i< nbpts_connus; i++ ){
+            // La table est initialisée avec les valeurs y0 connus , donc f(x0)
+            Point_Connu = tabpts_connus[i];
+            tab_diff[i] = Point_Connu.ordonnee();
+        }
+        System.out.println("---Table des coefficients--- \n");
+        /**
+         * Calcul des coefficients du polynôme d'interpolation.
+         * Le premier coefficient contenu dans la case 0 de table_ceof[]
+         * est initialisé par le contenu de la case 0 de table_dif[], soit la valeur de f(x0)
+         */
+        tab_coef[0] = tab_diff[0];
+        System.out.printf("k=%d table_coef[%d]=%f\n",0,0, tab_coef[0]);
+        /**
+         * Boucle de compteur k calcul les coefficients successifs qui sont rangés dans la table_coef[]
+         */
+        for (k = 1; k < nbpts_connus; k++){
+            /**
+             * boucle de compteur i calcule chaque différence divisée de cette itération
+             */
+            for (i = 0 ; i < nbpts_connus ; i++) {
+                j = i + k;
+                Point_Connu_j = tabpts_connus[j];
+                Point_Connu_i = tabpts_connus[i];
+                tab_diff[i] = (tab_diff[i + 1] - tab_diff[i]) / (Point_Connu_j.abcisse() - Point_Connu_i.abcisse());
+            }
+                /**
+                 *Une fois toutes les différences de cette itération calculées, on range le contenu de la case 0 de la table des différence
+                 * dans la case k de la table des coefficients
+                 */
+                tab_coef[k] = tab_diff[0];
+                System.out.printf("k=%d  table_coef[%d]=%f\n",k,k,tab_coef[k]);
+            }
+            /**
+             * Calcul polynôme d'interpolation pour les points à interpoler.
+             * La boucle k construit chaque termes du polynôme et l'additionne au calcul précédent.
+             */
+            System.out.println("--- Calcul du polynôme ---\n");
+            for(k =0; k < nbpts_a_interpoler; k++){
+                CPoint Point_a_Interpoler = new CPoint();
+                Point_a_Interpoler = tabpts_a_interpoler[k];
+                double abscisse_pt_a_Interpoler = Point_a_Interpoler.abcisse();
+                double ordonnee_pt_a_Interpoler = Point_a_Interpoler.ordonnee();
+                for (j = 0; j < nbpts_connus; j++){
+                    /**
+                     * On évalue chaque terme du polynôme, par exemple: pour j=2 :
+                     * f[x0,x1,x2]*(z-x0)*(z-x1)
+                     * On récupère d'adord le coefficient de ce terme ,
+                     * ensuite boucle de calcul (z-x0)*(z-x1)*....
+                     */
+                    terme = tab_coef[j];
+                    for (i = 0 ; i < j; i++){
+                        Point_Connu = tabpts_connus[i];
+                        double abscisse_pt_connu = Point_Connu.abcisse();
+                        /**
+                         * On multiplie la valeur actuelle du terme par (z-xi) pour construire
+                         * ((z-x0)*(z-x1)*....*(z-xi)
+                         */
+                        terme = terme*(abscisse_pt_a_Interpoler - abscisse_pt_connu);
+                    }
+                    /**
+                     * On additionne la valeur du terme trouvé à la valeur actuelle du polynôme
+                     * donc à la valeur de pn(z)
+                     */
+                    ordonnee_pt_a_Interpoler = ordonnee_pt_a_Interpoler + terme;
+                    Point_a_Interpoler.affecte_point(abscisse_pt_a_Interpoler, ordonnee_pt_a_Interpoler);
+                    tabpts_a_interpoler[k] = Point_a_Interpoler;
+                    System.out.printf("j=%d  valeur de p(%f) = %f\n", j, Point_a_Interpoler.abcisse(), Point_a_Interpoler.ordonnee());
+                }
+            }
+        }
+    }
+
